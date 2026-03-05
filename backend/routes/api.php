30a01a602a631b83a90e4f485b33c99d9dc4f026
Ndoumbe <client@MacBook-Air-de-Ndoumbe.local>
2026-03-05 17:26:17 +0100

    feat: add notification and profile picture routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\NotificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ Route::middleware('api')->group(function () {
     Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
     Route::post('/reset-password', [AuthController::class, 'resetPassword']);
     
+    // Posts avec recherche et filtrage
     Route::get('/posts', [PostController::class, 'index']);
     Route::get('/posts/{id}', [PostController::class, 'show']);
     Route::get('/posts/{id}/comments', [CommentController::class, 'index']);
@@ -22,19 +24,31 @@ Route::middleware('api')->group(function () {
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     
+    // Profil utilisateur
     Route::get('/users/profile', [UserController::class, 'profile']);
     Route::put('/users/profile', [UserController::class, 'updateProfile']);
+    Route::put('/users/profile-picture', [UserController::class, 'updateProfilePicture']);
     Route::put('/users/password', [UserController::class, 'changePassword']);
     Route::delete('/users/profile', [UserController::class, 'deleteAccount']);
     
+    // Posts
     Route::post('/posts', [PostController::class, 'store']);
     Route::put('/posts/{id}', [PostController::class, 'update']);
     Route::delete('/posts/{id}', [PostController::class, 'destroy']);
     
+    // Commentaires
     Route::post('/posts/{post_id}/comments', [CommentController::class, 'store']);
     Route::put('/comments/{id}', [CommentController::class, 'update']);
     Route::delete('/comments/{id}', [CommentController::class, 'destroy']);
     
+    // Likes
     Route::post('/posts/{post_id}/like', [LikeController::class, 'store']);
     Route::delete('/posts/{post_id}/like', [LikeController::class, 'destroy']);
+    
+    // Notifications
+    Route::get('/notifications', [NotificationController::class, 'index']);
+    Route::get('/notifications/unread-count', [NotificationController::class, 'unreadCount']);
+    Route::put('/notifications/{id}/read', [NotificationController::class, 'markAsRead']);
+    Route::put('/notifications/read-all', [NotificationController::class, 'markAllAsRead']);
 });
+
